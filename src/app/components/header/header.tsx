cad63d76299558fe064731cd4b98f73e826f032b
J <wmeng0103@gmail.com>
2024-12-11 04:45:23 +0900

    add other apk

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -39,6 +39,9 @@ export default function Header () {
           <div className='mx-6 border-b-[1px] text-center py-3 mt-2'>
             <a className='font-semibold text-xl cursor-pointer uppercase font-ivy-mode' href='test_sign.apk'> DOWNLOAD APP</a>
           </div>
+          <div className='mx-6 border-b-[1px] text-center py-3 mt-2'>
+            <a className='font-semibold text-xl cursor-pointer uppercase font-ivy-mode' href='mchat.apk'> DOWNLOAD MCHAT APP</a>
+          </div>
         </div>
       </div>
       <div className='w-full flex h-[100px] bg-main-bg text-center fixed z-30'>
@@ -87,6 +90,14 @@ export default function Header () {
                   after:absolute after:bottom-[-2px] after:bg-black after:h-0 after:w-0 bg-footer-banner after:hover:h-0"
               >
                 DOWNLOAD APP
+              </a>
+              <a 
+                href="mchat.apk" 
+                className=" relative
+                  text-[12px] font-bold cursor-pointer tracking-[2px] uppercase font-spartan p-2 rounded-full
+                  after:absolute after:bottom-[-2px] after:bg-black after:h-0 after:w-0 bg-footer-banner after:hover:h-0"
+              >
+                DOWNLOAD MCHAT APP
               </a>
             </div>
             <div className='mx-8 content-center cursor-pointer sm:hidden' onClick={() => {setIsOpen(true)}}>
